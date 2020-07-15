f93ba80c1bfc0c0f55cb12cd21c5887d75814b15
Abhishek <abugautam@gmail.com>
2020-07-15 09:46:50 +0530

    Added subject and service for posts get and save

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,16 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector : 'app-post-list',
   templateUrl : './post-list.component.html',
   styleUrls : ['./post-list.component.css']
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit, OnDestroy {
   // posts = [
   //   {title : 'First Post', content: 'This is first post content'},
   //   {title : 'Second Post', content: 'This is second post content'},
   //   {title : 'Third Post', content: 'This is third post content'}
   // ];
-  @Input() posts : Post[] = [];
+  posts : Post[] = [];
+  private postsSub : Subscription;
+
+  constructor(private postsService : PostsService) {}
+
+  ngOnInit() {
+    this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener()
+      .subscribe((posts : Post[]) => {
+        this.posts = posts;
+      });
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
 }
