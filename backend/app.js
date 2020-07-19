b235809bc5ef5c80439f9f320db5ca78d95c61b8
Abhishek <abugautam@gmail.com>
2020-07-19 19:32:49 +0530

    Added edit and update route  plus endpoint on server

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,19 @@ app.post("/api/posts", (req, res, next) => {
     });
   });
 
-})
+});
+
+app.put("/api/posts/:id", (req, res, next) => {
+  const post = new Post({
+    _id: req.body.id,
+    title : req.body.title,
+    content : req.body.content
+  })
+  Post.updateOne({_id: req.params.id}, post).then(result => {
+    console.log(result);
+    res.status(200).json({ message: "update successful" });
+  })
+});
 
 app.get('/api/posts', (req, res, next) => {
   Post.find().then(documents => {
